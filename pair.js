9f283c02e3530288dbf3ed2f6e7ce12660d35c7e
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrelltakudzwa9@gmail.com>
2025-08-27 16:21:04 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -21,7 +21,7 @@ const megaCredentials = [
 */
 // Array of Mega credentials
 const megaCredentials = [
-  { email: 'dgdarrell8@gmail.com', password: 'subzero25' }
+  { email: 'dgdarrell8@gmail.com', password: 'darex000' }
 ];
 
 
